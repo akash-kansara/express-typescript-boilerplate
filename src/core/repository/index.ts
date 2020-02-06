cbb9fc2b7269eb3cb12680fc0261fc5946eaf295
Akash Kansara <Akash.Kansara@ril.com>
2020-02-06 16:33:02 +0530

    Removed unnecessary file

--- src/core/repository/index.ts
+++
@@ -1,8 +0,0 @@
-import { myContainer } from '../../di/di-config';
-import { TYPES } from '../../di/types';
-
-import IRepository from './definition';
-
-let repository: IRepository = myContainer.get<IRepository>(TYPES.IRepository);
-
-export default repository;
